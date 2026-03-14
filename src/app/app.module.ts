f8f5a563cf34c009ae6dc1148efbd068f5979b01
unknown <ricvera@favoritafc.com>
2026-03-14 11:16:46 -0500

    Agrega Dockerfile y workflow manual para publicar imagen frontend Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { OrderService } from './order.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule],
+  providers: [OrderService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
